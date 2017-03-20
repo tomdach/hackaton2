0c95a5e070f47b0927048516b1032d763a559279
Spac3G0at <victor.s.marie@gmail.com>
2017-03-20 15:06:58 +0100

    firstcommit

---
+++ hackathon2.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Lun 20 Mars 2017 à 14:58
+-- Version du serveur :  5.7.17-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.15-0ubuntu0.16.04.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `hackathon2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `titre` text NOT NULL,
+  `texte` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` tinytext NOT NULL,
+  `password` tinytext NOT NULL,
+  `mail` tinytext NOT NULL,
+  `dateNaiss` tinytext NOT NULL,
+  `dateInscription` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastCo` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nbrCom` int(11) NOT NULL DEFAULT '0',
+  `connected` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `mail`, `dateNaiss`, `dateInscription`, `lastCo`, `nbrCom`, `connected`) VALUES
+(1, 'root', 'admin', 'mail@mail.fr', '01/01/1990', '2017-03-20 10:52:46', '2017-03-20 11:52:46', 0, 'non');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ pages/login.php
@@ -0,0 +1,31 @@
+<?php 
+
+include_once("connexion.php");
+session_start();
+
+$username = $_POST["username"];
+
+echo $username;
+
+$req = mysqli_query($cnx, "SELECT * FROM users WHERE username= '$username'");
+$data = mysqli_fetch_assoc($req);
+
+if ($data["username"] == $_POST["username"] && $data["password"] == $_POST["password"] ) {
+		$_SESSION["connected"] = "yes";
+		$_SESSION["user"] = $username;
+		$_SESSION["message"] = $username;
+		// mysqli_query($cnx, "UPDATE lastCo SET lastCo = CURRENT_TIMESTAMP WHERE username = '$username'");
+	}else{
+		$_SESSION["message"] = "Identifiants ou mot de passe incorrect.";
+	}
+
+
+	$req1 = mysqli_query($cnx, "SELECT * FROM theme WHERE id=1");
+	$theme = mysqli_fetch_assoc($req1);
+	mysqli_query($cnx, "UPDATE theme SET login = 'non', signIn = 'non' WHERE id = 1");
+
+	header("location:../index.php");
+
+	// lastCo ne s'update pas
+
+?>
