912b5510550b6ffeaab0880ca6db210de6773612
Christopher62 <christopher.dachicourt@gmail.com>
2017-03-20 14:29:51 +0100

    ajout fichiers christopher

---
+++ christopher/FAQ.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/christopher.css">
+    <link rel="icon" href="http://boulogne.simplon.co/wp-content/uploads/2015/11/cropped-square-32x32.png">
+    <title>Foire Aux Questions</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row text-center col-md-offset-1 col-md-10">
+        <p>1. Qu'est ce que Simplon(.co) ?</p>
+        <p>Simplon.co est une start-up basée à Montreuil (Seine Saint-Denis) dans le domaine de l'apprentissage de la programmation et du numérique. Fondée notemment par Erwan Kezzar et Frédéric Bardeau en 2013, la start-up permet à des personnes avec des profils peu courants dans la profession (sans diplômes, des personnes avec une déficience physique, des jeunes issus de quartiers défavorisés) de se former à la production de code informatique.</p>
+        <p>2. Quelles sont les formations proposées ?</p>
+        <p>Plusieurs formations y sont dispensées dont celle du développement Web.</p>
+      </div>
+    </div>
+    <a href="#" id="toTop" class="fa fa-chevron-up fa-2x"></a>
+    <script src="https://use.fontawesome.com/8d66db2fbe.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/hackton.js"></script>
+  </body>
+</html>
