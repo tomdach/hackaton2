da214d90baab5fa33fc978870ef899fa2144a273
Christopher62 <christopher.dachicourt@gmail.com>
2017-03-21 15:48:18 +0100

    ajout fichiers christopher

---
+++ christopher/pages/navbar2.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="../victor.php"><img id="logo" src="../img/logo.png"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="../victor.php">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      	<li><?php
+              if (isset($_SESSION["user"])) {
+                echo "<a href=\"profile.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Profile</a>";
+              }
+              else{
+                echo "<a href=\"inscription.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Sign In</a>";
+              }
+            ?>
+          
+        </li>
+        <li>
+          <?php 
+            if (isset($_SESSION["user"])) {
+              echo "<a href=\"logout.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Logout</a>";
+            }
+            else{
+              echo "<a href=\"pageConnexion.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a>";
+            }
+          ?>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
