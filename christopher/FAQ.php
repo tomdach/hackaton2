dc9a0242f9ddd3f70d8f596e817850d71b925b1f
Christopher62 <christopher.dachicourt@gmail.com>
2017-03-22 10:28:50 +0100

    ajout fichiers christopher

--- christopher/FAQ.php
+++ christopher/FAQ.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,8 +13,49 @@
     <link rel="icon" href="http://boulogne.simplon.co/wp-content/uploads/2015/11/cropped-square-32x32.png">
     <title>Foire Aux Questions</title>
   </head>
-  <?php include_once("pages/navbarIndex.php") ;?>
   <body>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="../index.php"><img id="logo" src="img/logo.png"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="../index.php">Home</a></li>
+        <li><a href="actus.php">Actualités</a></li>
+        <li><a href="FAQ.php">FAQ</a></li>
+        <li><a href="../chris/chris.php">Contact</a></li>
+        <li><a href="../chris/forum.php">Forum</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><?php
+              if (isset($_SESSION["user"])) {
+                echo "<a href=\"pages/profile.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Profile</a>";
+              }
+              else{
+                echo "<a href=\"pages/inscription.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Sign In</a>";
+              }
+            ?>
+        </li>
+        <li>
+          <?php
+            if (isset($_SESSION["user"])) {
+              echo "<a href=\"pages/logout.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Logout</a>";
+            }
+            else{
+              echo "<a href=\"pages/pageConnexion.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a>";
+            }
+          ?>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <h1 id="titleFAQ">Foire Aux Questions !</h1>
     <h2 style="text-align:center">Présentation Simplon :</h2>
     <div class="container-fluid" id="FAQ">
