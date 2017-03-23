9abd8fda46d06a23d9dea37b2e5cacd5b54aacca
Spac3G0at <victor.s.marie@gmail.com>
2017-03-23 14:16:31 +0100

    third_commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-include_once("connexion.php");
+include_once("pages/connexion.php");
 session_start();
 ?>
 <!DOCTYPE html>
@@ -15,58 +15,12 @@ session_start();
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="index.php"><img id="logo" src="img/logo.png"></a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><?php
-              if (isset($_SESSION["user"])) {
-                echo "<a href=\"pages/profile.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Profile</a>";
-              }
-              else{
-                echo "<a href=\"pages/inscription.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Sign In</a>";
-              }
-            ?>
-          
-        </li>
-        <li>
-          <?php 
-            if (isset($_SESSION["user"])) {
-              echo "<a href=\"pages/logout.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Logout</a>";
-            }
-            else{
-              echo "<a href=\"pages/pageConnexion.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a>";
-            }
-          ?>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-<!-- <a href="pages/pageConnexion.php"><span class="glyphicon glyphicon-log-in"></span> Login</a> -->
-
-<!-- FIN NAVBAR -->
+<?php include("pages/navbarIndex.php"); ?>
   
 <div class="container-fluid text-center fill">    
   <div class="row content">
     <div class="col-sm-2 sidenav fill">
-      <p><a href="#">Link</a></p>
+      <p><a href="#">Articles</a></p>
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
     </div>
@@ -75,21 +29,43 @@ session_start();
       <div class="text-center">
       	<img class="img-responsive" id="logoIndex" src="img/simplonBSM.png">
       </div>
+      <hr>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </p>
       <hr>
-      <div>
-      <h3>Article 1</h3>
-      <div>
-      	<img class="articlesImg img-responsive" src="img/template.png">
-      </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo...</p>
-      <a href="#">Lire la suite</a>
-      </div>
-      <hr>
+
+      <?php 
+            $resul = mysqli_query($cnx,"SELECT * FROM articles");
+            $result = mysqli_fetch_assoc($resul);
+            $fnbr = $result["id"];
+            $fnbr;
+
+            $rez = mysqli_query($cnx, "SELECT MAX(id) FROM articles");
+            $rezu = mysqli_fetch_assoc($rez);
+            $max = $rezu["MAX(id)"];
+            $max++;
+
+            for ($i=$fnbr; $i < $max; $i++) {
+            
+            $resu = mysqli_query($cnx,"SELECT * FROM articles WHERE id=".$i."");
+            $don = mysqli_fetch_assoc($resu);
+
+                echo '<div class="row">
+                        <h3>'.$don["titre"].'</h3>
+                        <div class="col col-s-12 col-md-4 col-xs-4">
+                          <img class="articlesImg img-responsive" src="img/articles/'.$don["photo"].'">
+                        </div>
+                        <div class="col col-s-12 col-md-8 col-xs-8">
+                        <p>'.substr($don["texte"], 0, 300).'...</p>
+                        </div>
+                        <a style="float: right;" href="pages/article.php?id='.$don["id"].'">Lire la suite...</a>
+                        </div>
+                        <hr>
+                      ';
+            }
+      ?>
+
     </div>
     <div class="col-sm-2 sidenav fill">
       <div class="well">
